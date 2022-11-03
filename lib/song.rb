39d973cbb19db309713699d10a778eaa51d5bfcf
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-11-03 14:52:12 +0300

    completed

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,38 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id:nil)
     @name = name
     @album = album
   end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      album TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-sql
+      insert into songs(name,album)
+      values (?,?)
+    sql
+    DB[:conn].execute(sql,self.name,self.album)
+
+    self.id = DB[:conn].execute("Select last_insert_rowid() from songs")[0][0]
+    self
+  end
+
+  def self.create(name:,album:)
+    song = Song.new(name: name, album: album,)
+    song.save
+  end
+  self
 
 end
